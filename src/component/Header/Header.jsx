6970591bf7235a48d9b24960b641a54ac00252b4
Love Olubummo <olubummolove3@gmail.com>
2023-01-31 05:34:47 +0100

    feature:implemented hero animation

--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -1,11 +1,42 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./header.scss";
+import image1 from "../../assets/images/woman-with-satisfied-expression.png";
 
 const Header = () => {
+  const navClasses = ["nav1", "nav2", "nav3", "nav4", "nav5"];
+  const heroClasses = [
+    "header__hero1",
+    "header__hero2",
+    "header__hero3",
+    "header__hero4",
+    "header__hero5",
+  ];
+  const imgClasses = [
+    "header__hero-img1",
+    "header__hero-img2",
+    "header__hero-img3",
+    "header__hero-img4",
+    "header__hero-img5",
+  ];
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    return () => {
+      setInterval(() => {
+        setCounter((prev) => prev + 1);
+      }, 10000);
+    };
+  }, []);
+  useEffect(() => {
+    return () => {
+      if (counter === 4) setCounter(0);
+    };
+  }, [counter]);
+
   return (
     <>
       <header>
-        <nav>
+        <nav className={`${navClasses[counter]}`}>
           <div className="header__logo">TSAAD</div>
           <div className="header__nav">
             <div>Courses</div>
@@ -14,9 +45,15 @@ const Header = () => {
             <div>About Us</div>
           </div>
         </nav>
-        <div className="header__hero">
-          <div className="header__hero-img"></div>
-          <div className="header__hero-content"></div>
+        {/* <div className="">{counter}</div> */}
+        <div className={`${heroClasses[counter]}`}>
+          <div className={`${imgClasses[counter]}`}></div>
+          <div className="header__hero-content">
+            CURIOSITY.
+            <br />
+            MEETS. <br />
+            DISCOVERY.
+          </div>
         </div>
       </header>
     </>
